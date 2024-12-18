5b2d882f133a6a265f07da38b07372c2026cefd4
prachileena <140172878+prachileena@users.noreply.github.com>
2024-12-18 12:46:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Welcome to DevOps Training'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
